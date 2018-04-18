9aac295a72d8c4fd87a012426d2fe13f944c4605
andychen <andy.chen@iisigroup.com>
2018-04-18 23:45:51 +0800

    implement insertationSort

---
+++ algorithm/src/main/java/algorithm/sort/InsertaionSort.java
@@ -0,0 +1,27 @@
+package algorithm.sort;
+
+import algorithm.sort.tool.SortHelper;
+
+/**
+ * @author AndyChen
+ * @version <ul>
+ *          <li>2018/4/18 AndyChen,new
+ *          </ul>
+ * @since 2018/4/18
+ */
+public class InsertaionSort implements Sort {
+
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] array) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j].compareTo(array[j - 1]) < 0) { // j < j-1 swap
+                    SortHelper.swap(array, j, j - 1);
+                } else { // j > j - 1 代表更前面的數也都小於當前j
+                    break;
+                }
+            }
+        }
+        return array;
+    }
+}
