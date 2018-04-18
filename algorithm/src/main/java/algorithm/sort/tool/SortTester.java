4cf34c0bbe3d0b5f45a79c84082c20e12389bf6b
andychen <andy.chen@iisigroup.com>
2018-04-18 23:47:44 +0800

    impl result verufy method and add insertationSort to test

--- algorithm/src/main/java/algorithm/sort/tool/SortTester.java
+++ algorithm/src/main/java/algorithm/sort/tool/SortTester.java
@@ -1,9 +1,11 @@
 package algorithm.sort.tool;
 
+import algorithm.sort.InsertaionSort;
 import algorithm.sort.SelectSort;
 import algorithm.sort.Sort;
 
 import java.util.Date;
+
 
 /**
  * @author AndyChen
@@ -17,17 +19,31 @@ public class SortTester {
     public static void main(String[] args) {
         SelectSort selectSort = new SelectSort();
         testSort(selectSort);
+        InsertaionSort insertaionSort = new InsertaionSort();
+        testSort(insertaionSort);
     }
 
     public static void testSort(Sort sortObj) {
-        int number = 100000;
-        Integer[] testArray = SortHelper.randamIntArray(number, number);
+        int number = 10000;
+        Integer[] array = SortHelper.randamIntArray(number, number);
         long startTime = new Date().getTime();
-        sortObj.sort(testArray);
+        sortObj.sort(array);
         long endTime = new Date().getTime();
+        boolean verify = verify(array);
+        assert verify;
+        if (!verify)
+            System.out.println("verify fail");
         String className = sortObj.getClass().getName();
         className = className.substring(className.lastIndexOf(".") + 1);
         System.out.println(className + " sort size: " + number + " array, spent " + ((endTime - startTime) / 1000.0) + " s");
+    }
+
+    public static <T extends Comparable<T>> boolean verify(T[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i].compareTo(array[i + 1]) > 0)
+                return false;
+        }
+        return true;
     }
 
 }
